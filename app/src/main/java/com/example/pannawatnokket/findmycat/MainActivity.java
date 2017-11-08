ff26f01d14c4244d40858d52b0e3becd7d5aaffe
Pannawat Nokket <goku.pun@hotmail.com>
2017-11-09 02:32:15 +0700

    set time out

--- app/src/main/java/com/example/pannawatnokket/findmycat/MainActivity.java
+++ app/src/main/java/com/example/pannawatnokket/findmycat/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.pannawatnokket.findmycat;
 
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
 import com.example.pannawatnokket.findmycat.adapter.GameAdapter;
 
 import java.util.ArrayList;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private GridView gridView;
+    private RoundCornerProgressBar timeProgressBar;
 
     private GameAdapter gameAdapter;
     private ArrayList<Integer> imageIntegerArrayList;
     private int columns;
+    private int time;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +31,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         imageIntegerArrayList = new ArrayList<>();
         columns = 2;
+        time = 10;
         setUI();
+        initTimeProgress();
         setListener();
         nextLevel();
     }
 
     private void setUI() {
         gridView = (GridView) findViewById(R.id.grid_view);
+        timeProgressBar = (RoundCornerProgressBar) findViewById(R.id.time_progress);
+    }
+
+    private void initTimeProgress() {
+        timeProgressBar.setProgressColor(Color.parseColor("#4CA59D"));
+        timeProgressBar.setProgressBackgroundColor(Color.parseColor("#E7E7E9"));
+        timeProgressBar.setMax(10);
+        timeProgressBar.setProgress(10);
     }
 
     private void setListener() {
@@ -45,11 +62,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         gameAdapter = new GameAdapter(MainActivity.this, imageIntegerArrayList, columns);
         gridView.setAdapter(gameAdapter);
         gridView.setNumColumns(columns);
+        countTime();
+    }
+
+    private void countTime() {
+        countDownTimer = new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeProgressBar.setProgress(time--);
+            }
+
+            public void onFinish() {
+                timeProgressBar.setProgress(time--);
+                Toast.makeText(MainActivity.this, "time out", Toast.LENGTH_LONG).show();
+            }
+        }.start();
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         columns++;
+        countDownTimer.cancel();
+        time = 10;
+        initTimeProgress();
         nextLevel();
     }
 }
