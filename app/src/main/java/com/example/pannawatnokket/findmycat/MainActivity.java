30c067d4fd8010cd57cabc46472beaf2aade6093
Pannawat Nokket <goku.pun@hotmail.com>
2017-11-15 01:37:47 +0700

    set sound

--- app/src/main/java/com/example/pannawatnokket/findmycat/MainActivity.java
+++ app/src/main/java/com/example/pannawatnokket/findmycat/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.pannawatnokket.findmycat;
 
-import android.animation.ObjectAnimator;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
@@ -25,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private int columns;
     private float time;
     private CountDownTimer countDownTimer;
+    private MediaPlayer timeOutMediaPlayer;
+    private MediaPlayer theamSongMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
         imageIntegerArrayList = new ArrayList<>();
         columns = 2;
+        setSound();
         setUI();
         initTimeProgress();
         setListener();
@@ -41,12 +43,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private void setUI() {
         gridView = (GridView) findViewById(R.id.grid_view);
         timeProgressBar = (RoundCornerProgressBar) findViewById(R.id.time_progress);
+
+    }
+
+    private void setSound() {
+        timeOutMediaPlayer = MediaPlayer.create(this, R.raw.time_out);
+        theamSongMediaPlayer =  MediaPlayer.create(this, R.raw.theme_sound);
+        theamSongMediaPlayer.start();
+        theamSongMediaPlayer.setLooping(true);
     }
 
     private void initTimeProgress() {
         time = 10;
-        timeProgressBar.setProgressColor(Color.parseColor("#4CA59D"));
-        timeProgressBar.setProgressBackgroundColor(Color.parseColor("#E7E7E9"));
+        timeProgressBar.setProgressColor(Color.parseColor(getString(R.string.progress_color)));
+        timeProgressBar.setProgressBackgroundColor(Color.parseColor(getString(R.string.background_progress_color)));
         timeProgressBar.setMax(time);
         timeProgressBar.setProgress(time);
     }
@@ -72,13 +82,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             public void onTick(long millisUntilFinished) {
                 time = (float) (time - 0.037);
                 if (time < 5) {
-                    timeProgressBar.setProgressColor(Color.parseColor("#d65920"));
-                    timeProgressBar.setProgressBackgroundColor(Color.parseColor("#E7E7E9"));
+                    timeOutMediaPlayer.start();
+                    timeProgressBar.setProgressColor(Color.parseColor(getString(R.string.color_time_out)));
+                    timeProgressBar.setProgressBackgroundColor(Color.parseColor(getString(R.string.background_progress_color)));
                 }
                 timeProgressBar.setProgress(time);
             }
 
             public void onFinish() {
+                timeOutMediaPlayer.stop();
+                resetSound();
                 timeProgressBar.setProgress(0);
                 Toast.makeText(MainActivity.this, "time out", Toast.LENGTH_LONG).show();
             }
@@ -88,8 +101,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         columns++;
+        timeOutMediaPlayer.stop();
         countDownTimer.cancel();
+        resetSound();
         initTimeProgress();
         nextLevel();
+    }
+
+    private void resetSound() {
+        timeOutMediaPlayer.release();
+        timeOutMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.time_out);
     }
 }
