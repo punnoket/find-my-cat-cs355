0a01ad3f51c3edf7ae62a2571feb62559dd8bcbe
Pannawat Nokket <goku.pun@hotmail.com>
2017-11-17 01:59:03 +0700

    rename activity

--- app/src/main/java/com/example/pannawatnokket/findmycat/SplashActivity.java
+++ app/src/main/java/com/example/pannawatnokket/findmycat/SplashActivity.java
@@ -3,14 +3,9 @@ package com.example.pannawatnokket.findmycat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-import java.util.Timer;
-
-public class splashActivity extends Activity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +19,7 @@ public class splashActivity extends Activity {
 
             public void onFinish() {
 
-                        Intent intent = new Intent(splashActivity.this, HomeActivity.class);
+                        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                         startActivity(intent);
 
             }
