a75032d7102d7348a7cf10da6522906338f64c71
Pannawat Nokket <goku.pun@hotmail.com>
2017-11-19 16:15:57 +0700

    lower case

--- app/src/main/java/com/example/pannawatnokket/findmycat/sqlite/DatabaseManager.java
+++ app/src/main/java/com/example/pannawatnokket/findmycat/sqlite/DatabaseManager.java
@@ -72,7 +72,7 @@ public class DatabaseManager extends SQLiteOpenHelper {
     public ArrayList<User> getAllUser() {
         ArrayList<User> userArrayList = new ArrayList<>();
         sqLiteDatabase = this.getReadableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from " + TABLE_NAME, null);
 
         if (cursor.moveToFirst()) {
             while (cursor.moveToNext()) {
