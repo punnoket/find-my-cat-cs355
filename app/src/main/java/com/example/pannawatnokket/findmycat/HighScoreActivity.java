4596744f833cd0d5fa9333bbf8227d11d0473a19
Pannawat Nokket <goku.pun@hotmail.com>
2017-11-22 02:56:42 +0700

    show dialog score

--- app/src/main/java/com/example/pannawatnokket/findmycat/HighScoreActivity.java
+++ app/src/main/java/com/example/pannawatnokket/findmycat/HighScoreActivity.java
@@ -1,14 +1,18 @@
 package com.example.pannawatnokket.findmycat;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.pannawatnokket.findmycat.adapter.HighScoreAdapter;
 import com.example.pannawatnokket.findmycat.entity.User;
@@ -29,6 +33,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class HighScoreActivity extends Activity {
     private ShareDialog shareDialog;
     private CallbackManager callbackManager;
+    private Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,34 +54,8 @@ public class HighScoreActivity extends Activity {
                 startActivity(intent);
             }
         });
+        dialog = new Dialog(HighScoreActivity.this);
         callbackManager = CallbackManager.Factory.create();
-
-
-    }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-    }
-
-    private void showDialogSharedFacebook() {
-        ShareOpenGraphObject object = new ShareOpenGraphObject.Builder()
-                .putString("og:type", "books.book")
-                .putString("og:title", "คุณได้ "+getIntent().getIntExtra("score", 0)+"คะแนน")
-                .putString("og:description", "เก่งขนาดนี้ เหยี่ยวเรียกพ่อแล้วล่ะ")
-                .putString("og:image", "https://www.picz.in.th/images/2017/11/22/iconhome.png")
-                .putString("books:isbn", "0-553-57340-3")
-                .build();
-        ShareOpenGraphAction action = new ShareOpenGraphAction.Builder()
-                .setActionType("books.reads")
-                .putObject("book", object)
-                .build();
-        ShareOpenGraphContent content = new ShareOpenGraphContent.Builder()
-                .setPreviewPropertyName("book")
-                .setAction(action)
-                .build();
-        shareDialog.show(content);
-
         shareDialog = new ShareDialog(this);
         shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
             @Override
@@ -94,5 +73,59 @@ public class HighScoreActivity extends Activity {
 
             }
         });
+        showDialog();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+    private void showDialog() {
+        dialog.setContentView(R.layout.dialog_score);
+        dialog.setCancelable(false);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        LinearLayout done = dialog.findViewById(R.id.submit);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogSharedFacebook();
+                dialog.dismiss();
+            }
+        });
+
+        LinearLayout cancel = dialog.findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+        TextView scoreTextView = dialog.findViewById(R.id.score);
+        scoreTextView.setText(scoreTextView.getText().toString()+" "
+                +getIntent().getIntExtra("score", 0)
+                +getResources().getString(R.string.score));
+    }
+
+    private void showDialogSharedFacebook() {
+        ShareOpenGraphObject object = new ShareOpenGraphObject.Builder()
+                .putString("og:type", "books.book")
+                .putString("og:title", "คุณได้ "+getIntent().getIntExtra("score", 0)+getResources().getString(R.string.score))
+                .putString("og:description", "เก่งขนาดนี้ เหยี่ยวเรียกพ่อแล้วล่ะ")
+                .putString("og:image", "https://www.picz.in.th/images/2017/11/22/iconhome.png")
+                .putString("books:isbn", "0-553-57340-3")
+                .build();
+        ShareOpenGraphAction action = new ShareOpenGraphAction.Builder()
+                .setActionType("books.reads")
+                .putObject("book", object)
+                .build();
+        ShareOpenGraphContent content = new ShareOpenGraphContent.Builder()
+                .setPreviewPropertyName("book")
+                .setAction(action)
+                .build();
+        shareDialog.show(content);
     }
 }
